a2d193749d62a658f4d5cf65ec28f6331c3c6060
Maaacs <msb@icomp.ufam.edu.br>
2023-08-17 20:02:35 -0400

    Main

--- Grafo/grafo.c
+++ Grafo/grafo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 
 typedef struct ListaVizinhos{
@@ -84,9 +85,80 @@ bool grafo_atualizar_vizinhos(int tam, double raio_comunicacao, grafo_t grafo) {
     return true;
 }
 
+
+// Lê os nós do arquivo e preenhce o grafo
+bool ler_nohs(FILE *arquivo, int num_nos, grafo_t grafo) {
+    for (int i = 0; i < num_nos; i++) {
+        no_t no_atual;
+        int id;
+        double pos_x, pos_y;
+        if (fscanf(arquivo, "%d\t%lf\t%lf\n", &id, &pos_x, &pos_y) != 3) {
+            return false;
+        }
+        no_atual.id = id;
+        no_atual.x = pos_x;
+        no_atual.y = pos_y;
+        grafo[i] = no_atual;
+        grafo[i].lista = NULL;
+    }
+    return true; 
+}
+
+
+// Imprime o grafo e suas listas
 void grafo_imprimir(int tam, grafo_t grafo){
     for (int i = 0; i < tam; i++) {
-        printf("NO %d:  ", grafo[i].id);
+        printf("NO %d:  ", grafo[i].id); // Imprime o ID do nó
         lista_vizinhos_imprimir(*grafo[i].lista);
     }
+}
+
+
+// Libera a memória para o grafo e listas
+void free_grafo(int tam, grafo_t grafo) {
+    for (int i = 0; i < tam; i++) {
+        if (grafo[i].lista != NULL) {
+            lista_vizinhos_t *atual = *grafo[i].lista;
+
+            while (atual != NULL) {
+                lista_vizinhos_t *prox = atual->proximo; // Armazena o próximo antes de liberar
+                free(atual);
+                atual = prox; // Avança para o próximo nó
+            }
+
+            free(grafo[i].lista);
+            grafo[i].lista = NULL; // Limpa o ponteiro da lista
+        }
+    }
+    free(grafo);
+}
+
+
+
+
+int main(int argc, char *argv[]){
+
+    if (argc != 2) {
+        printf("Digite: %s <arquivo_entrada>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *arquivo = fopen(argv[1], "r");
+    if (!arquivo){
+        printf("Erro ao abrir o arquivo de entrada\n");
+    }
+    else{
+        int num_nos;
+        double raio;
+
+        fscanf(arquivo, "%d\t%lf\n", &num_nos, &raio);
+
+        grafo_t grafo = grafo_criar(num_nos);
+        ler_nohs(arquivo, num_nos, grafo);
+        grafo_atualizar_vizinhos(num_nos, raio, grafo);
+        grafo_imprimir(num_nos, grafo);
+        free_grafo(num_nos, grafo);
+    }
+
+    return 0;
 }
